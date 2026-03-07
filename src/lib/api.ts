77bf8e75b343f49434e8fd727cc87b4edd39f77f
mdsaif36 <156828844+mdsaif36@users.noreply.github.com>
2026-03-07 23:40:27 +0530

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Pointing to your Live Render Server
 const api = axios.create({
-  baseURL: 'http://localhost:3001', 
+  baseURL: 'https://prepnerveserver.onrender.com', 
   headers: {
     "Content-Type": "application/json",
   },
